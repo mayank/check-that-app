5f907d4789bf574b269e711b15ae97614fd16095
Mayank <pc@ubuntu.ubuntu-domain>
2020-12-07 11:29:45 +0530

    updated some charts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
-import { Typography, Row, Col, Card, Statistic } from 'antd';
-import { Area, Chart, Interval, Line } from 'bizcharts';
+import { Typography, Row, Col, Card, Statistic, Divider } from 'antd';
+import { Area, Chart, Interval, Line, registerShape } from 'bizcharts';
 import { useEffect, useRef, useState } from 'react';
 import { getServiceList, getServiceMetrics } from './request';
+
+
+registerShape('interval', 'border-radius', {
+  draw(cfg, container) {
+    const { points } = cfg;
+    let path = [];
+    path.push(['M', points[0].x, points[0].y]);
+    path.push(['L', points[1].x, points[1].y]);
+    path.push(['L', points[2].x, points[2].y]);
+    path.push(['L', points[3].x, points[3].y]);
+    path.push('Z');
+    path = this.parsePath(path);
+
+    const group = container.addGroup();
+    group.addShape('rect', {
+      attrs: {
+        x: path[1][1],
+        y: path[1][2],
+        width: path[2][1] - path[1][1],
+        height: path[0][2] - path[1][2],
+        fill: cfg.color,
+        radius: (path[2][1] - path[1][1]) / 2,
+      },
+    });
+
+    return group;
+  },
+});
 
 function App() {
 
@@ -30,7 +58,7 @@ function App() {
     updateMetrices();
     const intervalId = setInterval(() => {
       updateMetrices();
-    }, 10000);
+    }, 1000 * 60);
     return () => {
       clearInterval(intervalId);
     }
@@ -73,6 +101,7 @@ function App() {
                     autoFit
                     height={200}
                     data={serviceMetrics[services[k]._id]}
+                    scale={{value:{min: 0}}}
                   >
                     <Line
                       shape="smooth"
@@ -80,14 +109,31 @@ function App() {
                     />
                     <Area position="createdAt*time" />
                   </Chart>
+                  <Divider />
                   <Chart
                     autoFit
-                    height={50}
+                    height={100}
                     data={serviceMetrics[services[k]._id]}
+                    scale={{
+                      expected: {
+                        min:0,
+                        max:1,
+                        sync: "value"
+                      },
+                      actual: {
+                        sync: 'value',
+                      }
+                    }}
                   >
                     <Interval
-                      position="failures*time"
+                      position="failures*createdAt"
+                      color="#ddd"
+                      shape={['expected*createdAt'], (val) => 0}
+                    />
+                    <Interval
+                      position="failures*createdAt"
                       color="#a8071a"
+                      shape={['actual*createdAt'], (val) => val > 0 ? 1: 0}
                     />
                   </Chart>
                 </Card>
